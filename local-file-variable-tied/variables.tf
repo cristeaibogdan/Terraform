e925456ce135f2fefec721b6b6ef9e2b2a55095c
Bogdan <cristea.ibogdan@gmail.com>
2026-02-02 19:34:22 +0200

    Add resource interdependency

---
+++ local-file-variable-tied/variables.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+  default = "root/pets.txt"
+}
+
+variable "content" {
+  default = "We love pets!"
+}
+
+variable "prefix" {
+  default = "Mr"
+}
+
+variable "separator" {
+  default = "."
+}
+
+variable "lenght" {
+  default = 1
+}
